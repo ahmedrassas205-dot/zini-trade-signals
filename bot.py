6eb8689a7fcb78086f38a854fa76b9966da2bcbb
ahmedrassas205-dot <ahmedrassas205@gmail.com>
2026-07-05 04:52:01 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = 8813168774:AAGptYZfQkr0_OwJkslYx_4tYTEhUR05rrg
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "👋 أهلاً بك في Zini Trade Signals\n\n"
+        "البوت يعمل بنجاح.\n"
+        "قريباً سيبدأ بإرسال إشارات الذهب."
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
