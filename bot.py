8cb9a76c1526a080ebc6ae6bfd746a9660b5ee14
ahmedrassas205-dot <ahmedrassas205@gmail.com>
2026-07-05 19:01:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = 8813168774:AAGptYZfQkr0_OwJkslYx_4tYTEhUR05rrg
+TOKEN = os.getenv(8813168774:AAGptYZfQkr0_OwJkslYx_4tYTEhUR05rrg)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
